89a754b85e1c494f0260248092a4f759e6d4f4af
Ignacioc <63696926+IgnaC02@users.noreply.github.com>
2021-08-31 16:26:45 -0300

    Rutas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -71,24 +71,54 @@ router.get("/dogs", async (req, res) => {
 
 // GET /temperament:
 router.get("/temperament", async (req, res) => {
-  const temperamentApi = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`);
-  let temperaments = temperamentApi.data.map(ob => ob.temperament).toString();
-  temperaments = await temperaments.split(',');
+  const temperamentApi = await axios.get(
+    `https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}`
+  );
+  let temperaments = temperamentApi.data.map((ob) => ob.temperament).toString();
+  temperaments = await temperaments.split(",");
   // console.log('Test', temperaments)
-  const tempToDb = temperaments.forEach(ob => {
+  const tempToDb = temperaments.forEach((ob) => {
     Temperament.findOrCreate({
-      where: {name : ob}
-    })
-  })
+      where: { name: ob },
+    });
+  });
   const allTemperaments = await Temperament.findAll();
   res.send(allTemperaments);
 });
 
 // GET /dogs/{idRaza}
+router.get("/dogs/:id", async (req, res) => {
+  const id = req.params.id;
+  const dogsTotal = await getAllData();
+  if (id) {
+    let dogId = await dogsTotal.filter((ob) => ob.id == id);
+    dogId.length
+      ? res.status(200).json(dogId)
+      : res.status(404).send("Not found");
+  }
+});
 
-
-
-
-
+// POST /dog
+router.post("/dog", async (req, res) => {
+  // todo esto llega por body
+  let { name, temperaments, weight, height, life_span, image, createdDb } =
+    req.body;
+  // no le paso temperament, se lo hago a parte
+  let dogCreated = await Dog.create({
+    name,
+    weight,
+    height,
+    life_span,
+    image,
+    createdDb,
+  });
+  // se la encuentro a los temperament que busque en la base de datos todas las que coincidan con las de body
+  let temperamentDb = await Temperament.findAll({
+    where: { name: temperaments },
+  });
+  // agregado de temperamentos
+  dogCreated.addTemperaments(temperamentDb);
+  res.send("Your dog has been created!");
+});
 
 module.exports = router;
