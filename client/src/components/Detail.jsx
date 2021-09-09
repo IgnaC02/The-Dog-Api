a2248d5c971d8bd883c5db7a94967a0700918b58
Ignacioc <63696926+IgnaC02@users.noreply.github.com>
2021-09-09 00:45:49 -0300

    Detail

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../actions";
 import styles from "./Detail.module.css";
+import Loading from "../img/loading-4.gif"
 
 export default function Detail(props){
     const dispatch = useDispatch();
@@ -16,22 +17,23 @@ export default function Detail(props){
     const myDog = useSelector((state) => state.detail)
 
     return(
-        <div className={styles.container}>
+        <div className={styles.general}>
             {
                 myDog.length > 0 ?
                 <div className={styles.container}>
+                    <h1 className={styles.title}>{myDog[0].name}</h1>
                     <img src={myDog[0].image} alt="Img not found" className={styles.img}/>
-                    <h1>{myDog[0].name}</h1>
                     <h3> Weight: {myDog[0].weight} Kg</h3>
                     <h3> Height: {myDog[0].height} Cm</h3>
                     {/* <h3> Life span: {myDog[0].life_span} Years</h3> */}
                     <h3> Life span: {myDog[0].createdDb ? myDog[0].life_span + " years" : myDog[0].life_span} </h3>
                     <h3> Temperaments: {!myDog[0].createdDb? myDog[0].temperament + ' ' : myDog[0].temperaments.map(ob => ob.name + (' '))} </h3>
+                    <hr  className={styles.hr}/>
                     <Link to="/home">
-                        <button>Go back</button>
+                        <button className={styles.btn}>Go back</button>
                     </Link>
                 </div> : 
-                <p>Loading...</p>
+                <img src={Loading} alt="loading gift not found" className={styles.loading} />
             }
         </div>
     )
